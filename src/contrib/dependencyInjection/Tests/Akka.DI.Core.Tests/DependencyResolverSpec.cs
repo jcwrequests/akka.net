7ca0aa5ab7682031e780ff59441c77d1e6c14855
jcwrequests@comcast.net <jcwrequests@comcast.net>
2015-05-07 20:48:25 -0400

    Wired Up dependencies and added readme file

---
+++ src/contrib/dependencyInjection/Tests/Akka.DI.Core.Tests/DependencyResolverSpec.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Akka.Actor;
+using Akka.TestKit;
+using Xunit;
+
+namespace Akka.DI.Core.Tests
+{
+    public class DependencyResolverSpec : AkkaSpec
+    {
+        sealed class TestActorResolver : IDependencyResolver
+        {
+            private ActorSystem system;
+
+            public TestActorResolver(ActorSystem system)
+            {
+                if (system == null) throw new ArgumentNullException("system");
+                this.system = system;
+            }
+            public Type GetType(string actorName)
+            {
+                return typeof(TestActor);
+            }
+
+            public Func<ActorBase> CreateActorFactory(Type actorType)
+            {
+                Func<ActorBase> factory = () =>
+                {
+                    return (ActorBase)system.ActorOf(Props.Create(actorType));
+                };
+                return factory;
+            }
+
+            public Props Create<TActor>() where TActor : ActorBase
+            {
+                return system.GetExtension<DIExt>().Props(typeof(TActor));
+            }
+
+            public void Release(ActorBase actor)
+            {
+                actor = null
+            }
+        }
+
+        //[Fact]
+        //rest of spec here
+    }
+}
